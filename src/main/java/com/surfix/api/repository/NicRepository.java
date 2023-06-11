50c1379bd623e3d3b0f9e345ba516af3a19cd4c1
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2023-06-10 23:50:49 -0300

    feat: create nic repository

---
+++ src/main/java/com/surfix/api/repository/NicRepository.java
@@ -0,0 +1,7 @@
+package com.surfix.api.repository;
+
+import com.surfix.api.models.Nic;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NicRepository extends JpaRepository<Nic, Long> {
+}
