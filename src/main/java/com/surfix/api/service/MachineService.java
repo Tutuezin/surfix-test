335444bd6e21a5b5b0fecb0e6edf0b31b2445327
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2023-06-10 23:47:32 -0300

    feat: create machine service

---
+++ src/main/java/com/surfix/api/service/MachineService.java
@@ -0,0 +1,50 @@
+package com.surfix.api.service;
+
+import com.surfix.api.models.Disk;
+import com.surfix.api.models.Machine;
+import com.surfix.api.models.Nic;
+import com.surfix.api.repository.DiskRepository;
+import com.surfix.api.repository.MachineRepository;
+import com.surfix.api.repository.NicRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MachineService {
+    @Autowired
+    private MachineRepository machineRepository;
+    @Autowired
+    private DiskRepository diskRepository;
+    @Autowired
+    private NicRepository nicRepository;
+
+    public List<Machine> getAll() {
+        return machineRepository.findAll();
+    }
+
+    public Machine create(Machine req) {
+
+        Machine createMachine = new Machine();
+        createMachine.setName(req.getName());
+        createMachine.setMemory(req.getMemory());
+        createMachine.setVcpu(req.getVcpu());
+        createMachine.setState(req.getState());
+
+        for (Disk disk : req.getDisks()){
+            disk.setMachine(createMachine);
+            Disk saveDisk = diskRepository.save(disk);
+            createMachine.addDisk(saveDisk);
+        }
+
+        for (Nic nic : req.getNics()) {
+            nic.setMachine(createMachine);
+            Nic saveNic = nicRepository.save(nic);
+            createMachine.addNic(saveNic);
+        }
+
+        return machineRepository.save(createMachine);
+    }
+
+}
