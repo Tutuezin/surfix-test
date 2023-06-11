af7d8e07b5b54aa5aea6a67b2a85713a2ba0969f
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2023-06-11 02:19:03 -0300

    refac: machine dto with record

--- src/main/java/com/surfix/api/dto/MachineDTO.java
+++ src/main/java/com/surfix/api/dto/MachineDTO.java
@@ -7,23 +7,28 @@ import com.surfix.api.models.Nic;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.Data;
 
-/* public record MachineDTO(String name, Integer memory, Integer vcpu, Boolean state, @NotNull @NotBlank List<Disk> disks,
+public record MachineDTO(String name, Integer memory, Integer vcpu, Boolean state, @NotNull @NotBlank List<Disk> disks,
     @NotNull @NotBlank List<Nic> nics) {
 }
- */
 
-@Data
-public class MachineDTO {
-  private String name;
-  private Integer memory;
-  private Integer vcpu;
-  private Boolean state;
-  @NotNull
-  @NotBlank
-  private List<Disk> disks;
-  @NotNull
-  @NotBlank
-  private List<Nic> nics;
-}
+/* Outra forma de fazer o DTO sem record */
+/*
+ * @Data
+ * public class MachineDTO {
+ * private String name;
+ * private Integer memory;
+ * private Integer vcpu;
+ * private Boolean state;
+ * 
+ * @NotNull
+ * 
+ * @NotBlank
+ * private List<Disk> disks;
+ * 
+ * @NotNull
+ * 
+ * @NotBlank
+ * private List<Nic> nics;
+ * }
+ */
